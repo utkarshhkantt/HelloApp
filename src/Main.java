e2e39ed4253d9061a75861fe8fdb5d52ee7be837
utkarshhkantt <kanttukarsh@gmail.com>
2026-04-01 22:18:54 +0530

    UC5: Multiple names using enhanced for loop

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         } else {
-            System.out.println("Hello World");
+            System.out.println("Hello World!");
         }
 
     }
