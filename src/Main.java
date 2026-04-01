0fc4e558cafcc7b5bb94103f25ebc0c93e8cfa45
utkarshhkantt <kanttukarsh@gmail.com>
2026-04-01 22:34:37 +0530

    UC7: Multiple names using String.join()

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            String result = "Hello ";
-
-            for (String name : args) {
-                result += name + ", ";
-            }
-
-      
-            result = result.substring(0, result.length() - 2);
-
-            System.out.println(result);
-
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
         } else {
             System.out.println("Hello, World!");
         }
