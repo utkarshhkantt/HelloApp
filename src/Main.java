c14c4fd6352edde5ce9f2eb48027a97e04d8189d
utkarshhkantt <kanttukarsh@gmail.com>
2026-04-01 22:29:54 +0530

    UC6: Multiple names using substring to remove trailing comma

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length > 0) {
+            String result = "Hello ";
+
             for (String name : args) {
-                System.out.println("Hello " + name);
+                result += name + ", ";
             }
+
+      
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println(result);
+
         } else {
-            System.out.println("Hello World!");
+            System.out.println("Hello, World!");
         }
 
     }
