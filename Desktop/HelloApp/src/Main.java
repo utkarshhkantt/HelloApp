2d9236495a80a071fe527ea06cb893a1c5974ec8
utkarshhkantt <kanttukarsh@gmail.com>
2026-04-01 21:41:36 +0530

    UC2: Display name using command-line argument

--- Desktop/HelloApp/src/Main.java
+++ Desktop/HelloApp/src/Main.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("No name provided");
+        }
 
-        System.out.println("Hello " + name);
     }
 }
